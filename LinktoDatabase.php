391cde73fdf4a6b0b963a725bf01ee05b1e75c22
HenryXiang994 <111212493+HenryXiang994@users.noreply.github.com>
2022-08-20 22:39:38 +0800

    Add files via upload

---
+++ LinktoDatabase.php
@@ -0,0 +1,41 @@
+<?php
+
+//nettest
+//nettestform
+
+
+//itemid  （自动编号，不需要填写）
+//itemindex (TBD- 增加编码规则)
+
+//ReportDate
+//ReportEngineername
+//ReportPartName
+//ReportIssueType
+//ReportDescription
+
+//localhost:80/LinktoDatabase.php
+
+
+// 参数例子 from HPHadmin UPDATE `nettestform` SET `reportissuetype` = 'jhhfdhl', `reportdescription` = 'ggkfsakj' WHERE `nettestform`.`itemid` = 6;
+
+// INSERT INTO `nettestform` (`itemid`, `reportdate`, `reportengineername`, `reportpartpame`, `reportissuetype`, `reportdescription`) 
+//VALUES (NULL, '2022-08-20', 'Henry Xiang', 'Main key', 'vfvczv', 'vvdsafdsa');
+
+
+
+
+//!--                主机，   登录账号名称， 登录密码   SQL数据库名称 -->
+$conn = mysqli_connect("localhost","root", "","nettest");
+
+if (!$conn) {
+die("链接失败, 服务器未响应, 请联系Henry Xiang开启服务器 ". mysqli_connect_error());
+}
+echo "链接成功";
+
+$insert_SQL = "INSERT INTO `nettestform` () VALUES (NULL, '$_POST[CTDATE]', '$_POST[CTENGNAME]', '$_POST[CTISTYPE]', '$_POST[CTPTNAME]', '$_POST[CTDESCRIP]')";
+
+mysqli_query($conn, $insert_SQL,); 
+
+
+
+?>
